fac82f38f4fe9b7f4db14c3834694d7fe1f7e295
govsandhu <gc.sandhu@gmail.com>
2019-07-12 01:35:27 +0000

    Added foreign keys to Review table

---
+++ db/migrate/20190712010707_linking_reviews_to_products_and_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class LinkingReviewsToProductsAndUsers < ActiveRecord::Migration
+  def change
+    remove_column :reviews, :user_id
+    remove_column :reviews, :product_id
+    add_reference :reviews, :user, foreign_key: true
+    add_reference :reviews, :product, foreign_key: true
+  end
+end
